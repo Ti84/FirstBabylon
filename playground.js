a546b8a2681241dd75697e14ba4c3bbe14dcdc79
Shane Cook <shane.cook@gm.com>
2018-10-17 15:14:58 -0700

    materials!

--- playground.js
+++ playground.js
@@ -1,50 +1,42 @@
-// Wrap in event listener (Waiting for dom to load before running)
 window.addEventListener('DOMContentLoaded', function(){
-  const canvas = document.getElementById('canvas');
+  var canvas = document.getElementById('canvas');
 
-  const engine = new BABYLON.Engine(canvas, true);
+  var engine = new BABYLON.Engine(canvas, true);
 
-  const createScene = function(){
-      const scene = new BABYLON.Scene(engine);
+  var createScene = function(){
+      var scene = new BABYLON.Scene(engine);
       scene.clearColor = new BABYLON.Color3.White();
-      const box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
-      const box2 = BABYLON.Mesh.CreateBox("Box",4.0,scene);
-      const material = new BABYLON.StandardMaterial("material1", scene);
-      material.wireframe = true;
-      box2.material = material;
-      box2.position = new BABYLON.Vector3(0, 5 ,0);
-      const camera = new BABYLON.ArcRotateCamera('arcCamera', 
-      BABYLON.Tools.ToRadians(45),
-      BABYLON.Tools.ToRadians(45),
-      10.0, box.position, scene); // Rotational camera!
+      var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+      var camera = new BABYLON.ArcRotateCamera("arcCam",
+              BABYLON.Tools.ToRadians(45),
+              BABYLON.Tools.ToRadians(45),
+              10.0,box.position,scene);
 
-      // const camera = new BABYLON.FollowCamera("followCam",BABYLON.Vector3.Zero(), scene);
-      // camera.lockedTarget = box;
-      // camera.radius = 10;
-      // camera.heightOffset = 0;
-      // camera.setTarget(BABYLON.Vector3.Zero());
-
-      const light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 10, 0), scene);
-      light.diffuse = new BABYLON.Color3(1, 0, 0);
-
-      scene.actionManager = new BABYLON.ActionManager(scene);
-      scene.actionManager.registerAction(
-        new BABYLON.ExecuteCodeAction( { trigger: BABYLON.ActionManager.OnKeyUpTrigger, parameter: " " },
-        function() {
-          light.setEnabled(!light.isEnabled());
-        }));
       camera.attachControl(canvas,true);
+
+
+      var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 10, 0), scene);
+      light.parent = camera;
+      light.diffuse = new BABYLON.Color3(1,1,1);
+
+      var material = new BABYLON.StandardMaterial("material1",scene); // StandardMaterial - collection of textures!
+      //material.wireframe = true; // Its just a wireframe!
+      box.material = material;
+      material.diffuseTexture = new BABYLON.Texture("texture.png", scene);
+      // material.bumpTexture = new BABYLON.Texture("texture_normal.png", scene); // Need a normal map for this to work! (can grab image / map from https://www.gamefromscratch.com/post/2017/01/18/BabylonJS-Tutorial-Series-Materials.aspx)
+      // material.diffuseColor = BABYLON.Color3.Blue();
+      //material.emissiveColor = BABYLON.Color3.Red(); color of lit up object
+      //material.specularColor = BABYLON.Color3.Red(); // color of light reflecting off of object
+      //material.alpha = .1; // Transparency
       return scene;
   }
 
-  const scene = createScene();
+  var scene = createScene();
   engine.runRenderLoop(function(){
-      // scene.getMeshByName("Box").position.z -= 0.01; moves dat black box
-      // var light = scene.getLightByName("pointLight");
-      // light.diffuse.g += 0.01; changes light color over time!
-      // light.diffuse.b += 0.01;
-
-
+    // Reference to material named box 
+      // var material = scene.getMeshByName("Box").material;
+      // material.alpha -= 0.01;  // Transparency over time
+      // if (material.alpha <- 0) material.alpha = 1;
       scene.render();
   });
 });
